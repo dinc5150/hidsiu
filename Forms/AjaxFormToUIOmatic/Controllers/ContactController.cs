a56a309bb11e8c6a3bb8a4ee68f43a7f0fbeba0c
David Sheiles <dinc5150@users.noreply.github.com>
2016-12-05 14:48:19 +1100

    Create ContactController.cs

---
+++ Forms/AjaxFormToUIOmatic/Controllers/ContactController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCUmbraco.Models;
+using System.Net.Mail;
+using System.Net;
+
+
+    public class ContactController : Umbraco.Web.Mvc.SurfaceController
+    {
+        [HttpPost]
+        public ActionResult SendMail(Contact form)
+        {
+            string retValue = "There was an error submitting the form, please try again later.";
+            if (!ModelState.IsValid)
+            {
+                return Content(retValue);
+            }
+
+            if (ModelState.IsValid)
+            {
+                //Update your SMTP server credentials
+                using (var client = new SmtpClient { Host = "smtp.gmail.com", Port = 587, EnableSsl = true, 
+                    Credentials = new NetworkCredential("account@gmail.com", "password"), 
+                    DeliveryMethod = SmtpDeliveryMethod.Network})
+                {
+                    var mail = new MailMessage();
+                    mail.To.Add("account@gmail.com"); // Update your email address
+                    mail.From = new MailAddress(form.Email, form.Name);
+                    mail.Subject = String.Format("Request to Contact from {0}", form.CompanyName);
+                    mail.Body = form.Message;
+                    mail.IsBodyHtml = false;
+                    try 
+                    {            
+                        client.Send(mail);
+                        retValue = "Your Request for Contact was submitted successfully. We will contact you shortly.";
+                    }
+                    catch (Exception)
+                    {
+        
+                        throw;
+                    }
+                }
+            }
+            return Content(retValue);
+        }
+    }
